82d9028fb27d52d40fc1e1e666eff071c77d89a4
ornateKarishok <ornate.karishok@gmail.com>
2023-02-08 11:39:03 +0100

    Fixing error with connection

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,53 +1,16 @@
 package com.example.myapplication;
 
-import static android.Manifest.permission.INTERNET;
-
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String port = "3306";
-    private static final String Classes = "org.postgresql.Driver";
-    private static final String database = " apkh_meeting_voicerecord";
-    private static final String username = "apkh@localhost";
-    private static final String password = "";
-    private static final String ip = "185.221.108.136";
-    private static final String url = "jdbc:postgresql://" + ip + ":" + port + "/" + database;
-
-    private Connection connection = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void start(View view) {
-        ActivityCompat.requestPermissions(this, new String[]{INTERNET}, PackageManager.PERMISSION_GRANTED);
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        try {
-            Class.forName(Classes);
-            connection = DriverManager.getConnection(url, username, password);
-            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Class fail", Toast.LENGTH_SHORT).show();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Connected no", Toast.LENGTH_SHORT).show();
-        }
+        Database db = new Database();
     }
 }
